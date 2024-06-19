9e393ea6223c389a4fc55d60fa9b99c9178ac0a6
seanstephens17 <d00211442@student.dkit.ie>
2024-06-19 17:48:58 +0100

    Image Reformatting File

---
+++ ImageFileType.py
@@ -0,0 +1,18 @@
+# pip install Pillow
+
+import os, sys
+from PIL import Image
+
+# Make sure the image you want to change is in the root directory
+images = ['test.png']
+
+for inImage in images:
+    f, e = os.path.splitext(inImage)
+    outImage = f + '.jpg'
+    if inImage != outImage:
+        try:
+            with Image.open(inImage) as image:
+                in_rgb = image.convert('RGB')
+                in_rgb.save(outImage, 'JPEG')
+        except OSError:
+            print('Conversion failed for', inImage)
