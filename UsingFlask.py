e537dac54ae4cb6ed4314266179518d8abaad5f7
seanstephens17 <d00211442@student.dkit.ie>
2024-07-16 14:40:02 +0100

    More code in HTML File

--- UsingFlask.py
+++ UsingFlask.py
@@ -5,17 +5,7 @@ app = Flask(__name__)  # instance of a flask web app
 
 @app.route("/<name>")  # how to access this page
 def home(name):  # defining home page
-    return render_template("index.html", content=name, r=2)  # can put html in here
-
-
-@app.route("/<name>")  # pass in as a parameter to return value below e.g. could be used to get a post ID
-def user(name):
-    return f"Hello {name}!"
-
-
-@app.route("/admin")
-def admin():
-    return redirect(url_for("user", name="Admin!"))  # name of function you want to move to if unauthorized
+    return render_template("index.html", content=["tim", "joe", "bill"])  # can put html in here
 
 
 if __name__ == "__main__":  # run the app
