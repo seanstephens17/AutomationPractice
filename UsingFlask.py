df0f06b7a2894f8b2e12460cc2c9903dc6a6f44f
seanstephens17 <d00211442@student.dkit.ie>
2024-07-17 10:48:23 +0100

    removing content variable from index, no longer takes argument

--- UsingFlask.py
+++ UsingFlask.py
@@ -5,7 +5,7 @@ app = Flask(__name__)  # instance of a flask web app
 
 @app.route("/")  # how to access this page
 def home():  # defining home page
-    return render_template("index.html", content="Testing")  # can put html in here
+    return render_template("index.html")  # can put html in here
 
 
 if __name__ == "__main__":  # run the app
