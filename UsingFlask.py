a49c62416881b2071e6da93bf0e291402e99c481
seanstephens17 <d00211442@student.dkit.ie>
2024-07-17 10:34:44 +0100

    Adding base template and extending with index

--- UsingFlask.py
+++ UsingFlask.py
@@ -3,11 +3,11 @@ from flask import Flask, redirect, url_for, render_template
 app = Flask(__name__)  # instance of a flask web app
 
 
-@app.route("/<name>")  # how to access this page
-def home(name):  # defining home page
-    return render_template("index.html", content=["tim", "joe", "bill"])  # can put html in here
+@app.route("/")  # how to access this page
+def home():  # defining home page
+    return render_template("index.html", content="Testing")  # can put html in here
 
 
 if __name__ == "__main__":  # run the app
-    app.run()
+    app.run(debug=True)  # automatically detect any changes and update the running server
 
